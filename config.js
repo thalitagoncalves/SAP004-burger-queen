dc412298b27ac098d73b35981eb648e5b6385648
Thalita Gonçalves <thagsousa15@gmail.com>
2020-07-17 16:04:12 -0300

    Instalação do firebase

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDybZN6bfT-EBdLu9zqtce5CSb0i1aplr8",
+    authDomain: "burger-queen-a7469.firebaseapp.com",
+    databaseURL: "https://burger-queen-a7469.firebaseio.com",
+    projectId: "burger-queen-a7469",
+    storageBucket: "burger-queen-a7469.appspot.com",
+    messagingSenderId: "984565720303",
+    appId: "1:984565720303:web:439e0f4f98ff1aa640a71a",
+    measurementId: "G-4ET3MQ00HC"
+};
+
+firebase.initializeApp(firebaseConfig);
