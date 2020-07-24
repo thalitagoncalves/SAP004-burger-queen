114fd92815f33202601e8a0346af9244ba624b6a
Thalita Gonçalves <thagsousa15@gmail.com>
2020-07-24 18:09:31 -0300

    Inserção do background através do MUI

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles'
 
-const Home = () => (
-  <div>
-    <header>
-      <h1>Página Home</h1>
-    </header>
-    <p>
-      <Link to='/signup'>Ir para a página de Cadastro</Link>
-    </p>
-  </div>
-);
+const useStyles = makeStyles({
+  root: {
+    height: '150vh',
+    background: '#231812',
+    padding: '1px',
+  }
+})
 
-export default Home;
+function Home() {
+  const background = useStyles();
+  return (
+    <div className={background.root}>
+      <section>
+        <div>SEJA <strong>BEM</strong> VINDO</div>
+      </section>
+      <p>
+        <Link to='/signup'>Cadastro</Link>
+        <Link to='/hall'><button>Salão</button></Link>
+        <Link to='/kitchen'><button>Cozinha</button></Link>
+      </p>
+    </div>
+  )
+}
+
+
+export default Home
