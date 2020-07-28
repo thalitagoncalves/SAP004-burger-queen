ad2807af9ce8f0286e757a021bb48956a9a3a04d
Thalita Gonçalves <thagsousa15@gmail.com>
2020-07-28 14:50:32 -0300

    Alteração no caminho das rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './index.css';
 import { ThemeProvider } from '@material-ui/core';
 import Home from './Home';
+import SignUp from './SignUp';
+import KitchenLogin from './Kitchen-login';
+import HallLogin from './Hall-login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
   return (
     <ThemeProvider>
-      <Home />
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/signup' component={SignUp} exact />
+          <Route path='/kitchen-login' component={KitchenLogin} exact />
+          <Route path='/hall-login' component={HallLogin} exact />
+        </Switch>
+      </BrowserRouter>,
     </ThemeProvider>
   )
 }
