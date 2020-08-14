d8c1e5d72dc6a2024bad80969ad7d9fc47e99da3
Thalita Gonçalves <thagsousa15@gmail.com>
2020-08-14 12:47:01 -0300

    Funcionalidade de enviar pedidos como prontos

--- src/pages/Kitchen.js
+++ src/pages/Kitchen.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import firebase from '../config';
 import { useHistory } from 'react-router-dom';
-import { Box, Typography, Button, ListItemText } from '@material-ui/core'
+import { Box, Typography, Button, ListItemText } from '@material-ui/core';
 import Logo from '../assets/logo-branco-burger-queen.png';
 import HeaderStyle from '../styles/Header.styles';
-import useStylesCard from '../styles/Menu.styles'
+import useStylesCard from '../styles/Menu.styles';
+import buttonStyle from '../styles/Button.style'
 
 
 function Kitchen() {
 
   const history = useHistory();
   const classes = HeaderStyle();
+  const btnStyle = buttonStyle();
   const card = useStylesCard();
   const [requests, setRequests] = useState([]);
 
@@ -31,21 +33,25 @@ function Kitchen() {
   const showRequests = () => {
     firebase.firestore()
       .collection('requests')
-      .get()
-      .then((querySnapshot) => {
+      .onSnapshot((querySnapshot) => {
         const data = [];
         querySnapshot.forEach((doc) => {
-          data.push(doc.data())
+          data.push({ ...doc.data(), id: doc.id })
         })
-        setRequests(data)
+        setRequests(data.filter((e) => e.status !== 'Pronto'))
       })
-      .catch((err) => console.log(err))
   }
 
   useEffect(() => {
     showRequests()
   }, [requests])
 
+  const updateStatus = (newStatus, requestID) => {
+    firebase
+      .firestore()
+      .collection('requests')
+      .doc(requestID).update({ status: newStatus })
+  }
 
   return (
     <Box>
@@ -56,7 +62,7 @@ function Kitchen() {
         <Typography variant='h6'>Cozinheiro: {firebase.auth().currentUser.displayName}</Typography>
         <Button onClick={logOut}>Sair</Button>
       </Box>
-      <Box>
+      <Box display='flex' justifyContent='space-around'>
         {requests.map((item) => {
           return (
             <Box display='flex' alignItems='center' flexDirection='column' textAlign='center' width='250px' className={card.menu}>
@@ -66,6 +72,7 @@ function Kitchen() {
                   <ListItemText>{product.name} {product.quantity}x</ListItemText>
                 )
               })}</ListItemText>
+              <Button className={btnStyle.edit} variant="contained" onClick={() => updateStatus('Pronto', item.id)}>Pronto</Button>
             </Box>
           )
         })}
